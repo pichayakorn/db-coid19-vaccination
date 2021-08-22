5b4cc3f45544c6adb5ce1438f467344691163e88
pratch <pratch12652@gmail.com>
2021-08-22 19:48:01 +0700

    fix indent
    test fix indent with space

--- covid19-vaccination-schema-mysql.sql
+++ covid19-vaccination-schema-mysql.sql
@@ -28,18 +28,18 @@ DROP TABLE IF EXISTS
 ***************************CREATING TABLES************************
 **************************************************************** */
 CREATE TABLE person (
-	personal_id		INTEGER			NOT NULL,
-	first_name		VARCHAR(14)		NOT NULL,
-	last_name		VARCHAR(16)		NOT NULL,
-	birth_date		DATE			NOT NULL,
+	personal_id     INTEGER			NOT NULL,
+	first_name      VARCHAR(14)		NOT NULL,
+	last_name       VARCHAR(16)		NOT NULL,
+	birth_date      DATE			NOT NULL,
 	phone_number    VARCHAR(15),
 	PRIMARY KEY (personal_id)
 );
 
 CREATE TABLE location (
-	location_id		INTEGER			NOT NULL,
+	location_id     INTEGER			NOT NULL,
 	location_name   VARCHAR(50)		NOT NULL,
-	postal_code		VARCHAR(10)		NOT NULL,
+	postal_code     VARCHAR(10)		NOT NULL,
 	PRIMARY KEY (location_id)
 );
 
@@ -51,32 +51,32 @@ CREATE TABLE vaccine (
 );
 
 CREATE TABLE vaccine_storage (
-	lot_no			VARCHAR(10)		NOT NULL,
+	lot_no          VARCHAR(10)		NOT NULL,
 	vaccine_code    CHAR(5)			NOT NULL,
 	receive_date    DATE			NOT NULL,
-	expire_date		DATE			NOT NULL,
-	quantity		INTEGER			NOT NULL,
+	expire_date     DATE			NOT NULL,
+	quantity        INTEGER			NOT NULL,
 	FOREIGN KEY (vaccine_code) REFERENCES vaccine(vaccine_code),
 	PRIMARY KEY (lot_no)
 );
 
 CREATE TABLE service_center (
-	center_id		VARCHAR(10)		NOT NULL,
-	location_id		INTEGER			NOT NULL,
-	lot_no			VARCHAR(10)		NOT NULL,
+	center_id       VARCHAR(10)		NOT NULL,
+	location_id     INTEGER			NOT NULL,
+	lot_no          VARCHAR(10)		NOT NULL,
 	receive_date    DATE			NOT NULL,
-	quantity		INTEGER			NOT NULL,
+	quantity        INTEGER			NOT NULL,
 	FOREIGN KEY (location_id) REFERENCES location(location_id),
 	FOREIGN KEY (lot_no) REFERENCES vaccine_storage(lot_no),
 	PRIMARY KEY (center_id)
 );
 
 CREATE TABLE vaccination_record (
-	record_no		INTEGER			NOT NULL,
-	personal_id		INTEGER			NOT NULL,
+	record_no       INTEGER			NOT NULL,
+	personal_id     INTEGER			NOT NULL,
 	vaccinate_date  DATE			NOT NULL,
-	serial_no		INTEGER			NOT NULL,
-	center_id		VARCHAR(10)		NOT NULL,
+	serial_no       INTEGER			NOT NULL,
+	center_id       VARCHAR(10)		NOT NULL,
 	FOREIGN KEY (personal_id) REFERENCES person(personal_id),
 	FOREIGN KEY (center_id) REFERENCES service_center(center_id),
 	PRIMARY KEY (record_no)
